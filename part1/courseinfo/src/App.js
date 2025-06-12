214b27a9fc902c89de9aa747dc61cc01b08ffa0a
lucalana <lucalana13@gmail.com>
2025-06-12 17:53:22 -0300

    1.6: unicafe — 1º passo

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,60 +1,28 @@
-const Header = (props) => {
-  return (
-    <>
-      <h1>{props.course}</h1>
-    </>
-  )
-}
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part} {props.exercises}
-      </p>
-    </>
-  )
-}
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
-    </>
-  )
-}
-const Total = (props) => {
-  return (
-    <>
-       <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    </>
-  )
-}
+import { useState } from 'react'
+
+const Exibir = ({ contador }) => <div>{contador}</div>
+
+const Botao = ({ onClick, texto }) => <button onClick={onClick}>{texto}</button>
 
 const App = () => {
-   const course = {
-    name: 'Desenvolvimento de aplicação Half Stack',
-    parts: [
-      {
-        name: 'Fundamentos da biblioteca React',
-        exercises: 10
-      },
-      {
-        name: 'Usando props para passar dados',
-        exercises: 7
-      },
-      {
-        name: 'Estado de um componente',
-        exercises: 14
-      }
-    ]
-  }
+  const [cliques, setCliques] = useState({ 
+    esquerda: 0, direita: 0
+  })
+
+  const handleCliqueEsquerda = () => setCliques({ ...cliques, esquerda: cliques.esquerda + 1,  })
+
+  const handleCliqueDireita = () => setCliques({ ...cliques, direita: cliques.direita + 1 })
+
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total  parts={course.parts} />
+      {cliques.esquerda}
+      <button onClick={handleCliqueEsquerda}>Esquerda</button>
+      <button onClick={handleCliqueDireita}>Direita</button>
+      {cliques.direita}
+      <p>
+        Total cliques: {cliques.esquerda + cliques.direita}
+      </p>
     </div>
   )
 }
---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+const Botao = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const App = () => {
+  const [good, addGood] = useState(0)
+  const [neutral, addNeutra] = useState(0)
+  const [bad, addBad] = useState(0)
+
+
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+
+      <Botao onClick={() => addGood(good + 1)} text="good"/>
+      <Botao onClick={() => addNeutra(neutral + 1)} text="neutral"/>
+      <Botao onClick={() => addBad(bad + 1)} text="bad"/>
+
+      <h2>Statics</h2>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+
+    </div>
+  )
+}
+
+export default App
